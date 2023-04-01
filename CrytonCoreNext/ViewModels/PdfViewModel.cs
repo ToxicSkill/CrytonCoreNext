5344466b1de0500c71e79c533a542117432f632a
Adam <gizmooos8@wp.pl>
2023-04-01 16:50:55 +0200

    Draft - Reading EXIF data from PDF

--- CrytonCoreNext/ViewModels/PdfViewModel.cs
+++ CrytonCoreNext/ViewModels/PdfViewModel.cs
@@ -5,9 +5,11 @@ using CrytonCoreNext.Interfaces.Files;
 using CrytonCoreNext.Models;
 using CrytonCoreNext.PDF.Interfaces;
 using CrytonCoreNext.PDF.Models;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Wpf.Ui.Mvvm.Contracts;
 using IDialogService = CrytonCoreNext.Interfaces.IDialogService;
@@ -59,6 +61,38 @@ namespace CrytonCoreNext.ViewModels
             var protectedFile = new List<File>();
             await foreach (var file in base.LoadFiles(Static.Extensions.DialogFilters.Pdf))
             {
+
+
+
+
+                using (var fileStream = System.IO.File.Open(file.Path, System.IO.FileMode.Open, System.IO.FileAccess.Read))
+                {
+                    // Read the PDF file into a byte array
+                    byte[] buffer = new byte[fileStream.Length];
+                    fileStream.Read(buffer, 0, (int)fileStream.Length);
+
+                    // Search for the start and end of the metadata block
+                    int start = Encoding.ASCII.GetString(buffer).IndexOf("%PDF-", StringComparison.Ordinal);
+                    int end = Encoding.ASCII.GetString(buffer).LastIndexOf("%%EOF", StringComparison.Ordinal);
+
+                    // Extract the metadata block as a string
+                    string metadata = Encoding.ASCII.GetString(buffer, start, end - start);
+
+                    // Search for the metadata properties and extract their values
+                    string author = GetValueFromMetadata(metadata, "/Author");
+                    string title = GetValueFromMetadata(metadata, "/Title");
+                    string subject = GetValueFromMetadata(metadata, "/Subject");
+
+                    // Output the metadata to the console
+                    Console.WriteLine("Author: " + author);
+                    Console.WriteLine("Title: " + title);
+                    Console.WriteLine("Subject: " + subject);
+                }
+
+
+
+
+
                 var pdfFile = _pdfService.ReadPdf(file);
                 if (pdfFile != null)
                 {
@@ -101,6 +135,26 @@ namespace CrytonCoreNext.ViewModels
                 SelectedImageFile = ImageFiles.First();
             }
             Unlock();
+        }
+
+        private static string GetValueFromMetadata(string metadata, string propertyName)
+        {
+            int index = metadata.IndexOf(propertyName, StringComparison.Ordinal);
+
+            if (index < 0)
+            {
+                return string.Empty;
+            }
+
+            int startIndex = index + propertyName.Length + 1;
+            int endIndex = metadata.IndexOf('\n', startIndex);
+
+            if (endIndex < 0)
+            {
+                endIndex = metadata.Length;
+            }
+
+            return metadata.Substring(startIndex, endIndex - startIndex).Trim();
         }
     }
 }
