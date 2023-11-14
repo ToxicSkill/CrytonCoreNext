5610b322b9813d9d6775a8a285c04818525d899a
Adam <gizmooos8@wp.pl>
2023-11-14 20:30:03 +0100

    Add - Different brightness method

--- CrytonCoreNext/Drawers/ImageDrawer.cs
+++ CrytonCoreNext/Drawers/ImageDrawer.cs
@@ -1,4 +1,5 @@
-﻿using OpenCvSharp;
+﻿using Microsoft.Extensions.Logging;
+using OpenCvSharp;
 
 namespace CrytonCoreNext.Drawers
 {
@@ -13,8 +14,36 @@ namespace CrytonCoreNext.Drawers
             Cv2.Merge(channels, labColorMat);
             Cv2.CvtColor(labColorMat, labColorMat, ColorConversionCodes.Lab2LBGR);
             Cv2.AddWeighted(mat, 1 - strenght, labColorMat,  strenght, 0, labColorMat);
-            Cv2.ConvertScaleAbs(labColorMat, labColorMat, alpha, beta);
-            return labColorMat.Clone();
+            using var brightnessMat = SetBrightness(labColorMat, (int)beta);
+            return brightnessMat.Clone();
+        }
+
+        private static Mat SetBrightness(Mat mat, int brightness)
+        {
+            using var hsv = new Mat();
+            Cv2.CvtColor(mat, hsv, ColorConversionCodes.BGR2HSV);
+            var hsvChannles = Cv2.Split(hsv);
+            var lim = 255 - brightness;
+
+            for (var i = 0; i < hsv.Height; i++)
+            {
+                for (var j = 0; j < hsv.Width; j++)
+                {
+                    var value = hsvChannles[2].Get<byte>(i, j);
+                    if (value > lim)
+                    {
+                        hsvChannles[2].Set<byte>(i, j, 255);
+                    }
+                    else
+                    {
+                        hsvChannles[2].Set<byte>(i, j, (byte)(value+brightness));
+                    }
+                }
+            }
+
+            Cv2.Merge(hsvChannles, hsv); 
+            Cv2.CvtColor(hsv, mat, ColorConversionCodes.HSV2BGR);
+            return mat;
         }
     }
 }
