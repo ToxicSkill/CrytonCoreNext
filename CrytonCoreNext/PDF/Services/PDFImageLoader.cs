4477a888f2f398b57772aab467d952fe16d2c94a
Adam <gizmooos8@wp.pl>
2023-12-09 12:55:22 +0100

    Add - Resizing rednered mat

--- CrytonCoreNext/PDF/Services/PDFImageLoader.cs
+++ CrytonCoreNext/PDF/Services/PDFImageLoader.cs
@@ -13,6 +13,8 @@ namespace CrytonCoreNext.PDF.Services
 {
     public class PDFImageLoader : IPDFImageLoader
     {
+        private const int MaximalDimensionSize = 1500;
+
         public WriteableBitmap LoadImage(PDFFile pdfFile)
         {
             if (pdfFile.PdfStatus == Enums.EPdfStatus.Protected && string.IsNullOrEmpty(pdfFile.Password))
@@ -20,10 +22,7 @@ namespace CrytonCoreNext.PDF.Services
                 return default;
             }
             using IDocLib pdfLibrary = DocLib.Instance;
-            var dimensions = pdfFile.Dimensions;
-            using var reader = pdfFile.Password.Equals(string.Empty) ?
-                pdfLibrary.GetDocReader(pdfFile.Bytes, new PageDimensions(dimensions)) :
-                pdfLibrary.GetDocReader(pdfFile.Bytes, pdfFile.Password, new PageDimensions(dimensions));
+            using var reader = pdfLibrary.GetDocReader(pdfFile.Bytes, pdfFile.Password, new PageDimensions(pdfFile.Dimensions));
             using var docReader = reader;
             using var pageReader = docReader.GetPageReader(pdfFile.LastPage);
             using var mat = GetImage(pageReader);
@@ -37,10 +36,7 @@ namespace CrytonCoreNext.PDF.Services
                 yield return default;
             }
             using IDocLib pdfLibrary = DocLib.Instance;
-            var dimensions = pdfFile.Dimensions;
-            using var reader = pdfFile.Password.Equals(string.Empty) ?
-                pdfLibrary.GetDocReader(pdfFile.Bytes, new PageDimensions(dimensions)) :
-                pdfLibrary.GetDocReader(pdfFile.Bytes, pdfFile.Password, new PageDimensions(dimensions));
+            using var reader = pdfLibrary.GetDocReader(pdfFile.Bytes, pdfFile.Password, new PageDimensions(pdfFile.Dimensions));
             using var docReader = reader;
             for (var i = 0; i < pdfFile.NumberOfPages; i++)
             {
@@ -52,21 +48,34 @@ namespace CrytonCoreNext.PDF.Services
 
         private static Mat GetImage(IPageReader pageReader)
         {
-            var bgrBytes = pageReader.GetImage(RenderFlags.LimitImageCacheSize);
+            var bgrBytes = pageReader.GetImage(RenderFlags.OptimizeTextForLcd | RenderFlags.LimitImageCacheSize);
             var width = pageReader.GetPageWidth();
             var height = pageReader.GetPageHeight();
 
             using var bgraMat = new Mat(height, width, MatType.CV_8UC4, bgrBytes);
-            using var matOut = new Mat();
-
+            using var alphaMat = new Mat();
+            var newSize = new Size(width, height);
+            if (width > MaximalDimensionSize && width > height)
+            {
+                var ratio = (double)MaximalDimensionSize / (double)width;
+                var newHeight = height * ratio;
+                newSize = new Size(MaximalDimensionSize, newHeight);
+            }
+            else if (height > MaximalDimensionSize && width < height)
+            {
+                var ratio = (double)MaximalDimensionSize / (double)height;
+                var newWidth = width * ratio;
+                newSize = new Size(newWidth, MaximalDimensionSize);
+            }
             var nativeSplitted = Cv2.Split(bgraMat);
-            Cv2.Merge([nativeSplitted[3]], matOut);
-            using Mat inversed = new Scalar(255) - matOut;
-            var add = new Mat();
+            Cv2.Merge([nativeSplitted[3]], alphaMat);
+            using Mat inversedAlphaMat = new Scalar(255) - alphaMat;
+            var resultMat = new Mat();
             Cv2.CvtColor(bgraMat, bgraMat, ColorConversionCodes.BGRA2BGR);
-            Cv2.CvtColor(inversed, inversed, ColorConversionCodes.GRAY2BGR);
-            Cv2.Add(bgraMat, inversed, add);
-            return add;
+            Cv2.CvtColor(inversedAlphaMat, inversedAlphaMat, ColorConversionCodes.GRAY2BGR);
+            Cv2.Add(bgraMat, inversedAlphaMat, resultMat);
+            Cv2.Resize(resultMat, resultMat, newSize);
+            return resultMat;
         }
     }
 }
