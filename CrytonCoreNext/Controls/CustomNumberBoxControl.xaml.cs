d6ab881129c473eed11cac7ba1cdf1b15a8bef5f
Adam <61752657+ToxicSkill@users.noreply.github.com>
2023-12-16 20:58:10 +0100

    Add - Custom number box control

---
+++ CrytonCoreNext/Controls/CustomNumberBoxControl.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace CrytonCoreNext.Controls
+{
+    /// <summary>
+    /// Interaction logic for CustomNumberBoxControl.xaml
+    /// </summary> 
+    public partial class CustomNumberBoxControl : UserControl
+    {
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.Register("Value", typeof(int), typeof(CustomNumberBoxControl), new PropertyMetadata(0));
+
+
+        public int Value
+        {
+            get { return (int)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+
+        public CustomNumberBoxControl()
+        {
+            InitializeComponent();
+        }
+
+        private void SubtractValue(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            --Value;
+        }
+
+        private void AddValue(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            ++Value;
+        }
+    }
+}
