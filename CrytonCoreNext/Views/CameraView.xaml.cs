0dea5af6ea6a6f09b2c3ce16f4bde8c6048f34fc
Adam <gizmooos8@wp.pl>
2024-10-21 21:42:36 +0200

    Add - working plane particles animation

--- CrytonCoreNext/Views/CameraView.xaml.cs
+++ CrytonCoreNext/Views/CameraView.xaml.cs
@@ -5,6 +5,7 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Media.Effects;
 using System.Windows.Shapes;
+using Wpf.Ui.Appearance;
 using Wpf.Ui.Controls;
 
 namespace CrytonCoreNext.Views
@@ -29,36 +30,51 @@ namespace CrytonCoreNext.Views
             var width = canvasBorder.ActualWidth;
             var height = canvasBorder.ActualHeight;
             var rnd = new Random();
-            for (var i = 1; i < 4; i++)
+            var particles = 20;
+            var plans = 4;
+            for (var i = plans; i != 0; i--)
             {
-                for (var j = 0; j < 10; j++)
+                for (var j = 0; j < particles; j++)
                 {
                     var newX = rnd.Next(0, (int)width);
                     var newY = rnd.Next(0, (int)height);
                     var elipse = new Ellipse()
                     {
-                        Width = 5 + 15 / i,
-                        Height = 5 + 15 / i,
-                        Fill = new SolidColorBrush(Colors.Red),
-                        HorizontalAlignment = System.Windows.HorizontalAlignment.Left,
-                        VerticalAlignment = System.Windows.VerticalAlignment.Top,
+                        Opacity = 0,
+                        Width = 5 + i,
+                        Height = 5 + i,
+                        Fill = new SolidColorBrush(ApplicationAccentColorManager.PrimaryAccent),
+                        HorizontalAlignment = HorizontalAlignment.Left,
+                        VerticalAlignment = VerticalAlignment.Top,
                         Margin = new Thickness(newX, newY, 0, 0),
                         Effect = new BlurEffect()
                         {
-                            Radius = i * 5
-                        }
+                            Radius = 1 + 2 * i,
+                            RenderingBias = RenderingBias.Quality,
+                            KernelType = KernelType.Gaussian
+                        },
                     };
                     var sb = new Storyboard();
+                    var delay = rnd.Next(0, particles);
                     var ta = new ThicknessAnimation
                     {
-                        BeginTime = new TimeSpan(0),
+                        BeginTime = TimeSpan.FromSeconds(delay),
                         From = new Thickness(newX, newY, 0, 0),
                         To = new Thickness(newX + (50 * (rnd.Next(0, 2) == 0 ? (-1) : 1)), newY + (50 * (rnd.Next(0, 2) == 0 ? (-1) : 1)), 0, 0),
-                        Duration = new Duration(TimeSpan.FromSeconds(4)),
+                        Duration = new Duration(TimeSpan.FromSeconds(20)),
                         AutoReverse = true,
-                        RepeatBehavior = RepeatBehavior.Forever
+                        RepeatBehavior = RepeatBehavior.Forever,
+                        AccelerationRatio = 0.5,
+                        DecelerationRatio = 0.5
                     };
-
+                    elipse.BeginAnimation(OpacityProperty,
+                        new DoubleAnimation()
+                        {
+                            From = 0,
+                            To = 1,
+                            BeginTime = TimeSpan.FromSeconds(delay),
+                            Duration = TimeSpan.FromSeconds(10)
+                        });
                     Storyboard.SetTargetProperty(ta, new PropertyPath(MarginProperty));
                     sb.Children.Add(ta);
                     sb.Begin(elipse);
